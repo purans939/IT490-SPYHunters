6e36b5c32ce2bd1e7dbbb1d641fba2d285590376
dclerigo <140534171+dclerigo@users.noreply.github.com>
2023-10-30 09:00:48 -0400

    Add files via upload

--- front-end/authenticate.php
+++ front-end/authenticate.php
@@ -1,51 +1,39 @@
 <?php
 session_start();
-// Change this to your connection info.
-$DATABASE_HOST = '172.25.40.184';
-$DATABASE_USER = 'baseHost';
-$DATABASE_PASS = 'host';
-$DATABASE_NAME = 'baseDB';
-// Try and connect using the info above.
-$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
-if ( mysqli_connect_errno() ) {
-	// If there is an error with the connection, stop the script and display the error.
-	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
-}
 
 if ( !isset($_POST['username'], $_POST['password']) ) {
 	// Could not get the data that should have been sent.
 	exit('Please fill both the username and password fields!');
 }
 
-if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?')) {
-	// Bind parameters
-	$stmt->bind_param('s', $_POST['username']);
-	$stmt->execute();
-	// Store the result so we can check if the account exists in the database.
-	$stmt->store_result();
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
 
-    if ($stmt->num_rows > 0) {
-        $stmt->bind_result($id, $password);
-        $stmt->fetch();
-        // Account exists, now we verify the password.
-        if (password_verify($_POST['password'], $password)) {
-            // Verification success
-            // Create sessions, so we know the user is logged in
-            session_regenerate_id();
-            $_SESSION['loggedin'] = TRUE;
-            $_SESSION['name'] = $_POST['username'];
-            $_SESSION['id'] = $id;
-            echo 'Welcome ' . $_SESSION['name'] . '!';
-        } else {
-            // Incorrect password
-            echo 'Incorrect username and/or password!';
-        }
-    } else {
-        // Incorrect username
-        echo 'Incorrect username and/or password!';
-    }
-
-
-	$stmt->close();
+$client = new rabbitMQClient("testRabbitMQ.ini","testServer");
+if (isset($argv[1]))
+{
+  $msg = $argv[1];
 }
+else
+{
+  $msg = "test message";
+}
+
+$request = array();
+$request['type'] = "Login";
+$request['username'] = $_POST['username'];
+$request['password'] = $_POST['password'];
+$request['message'] = $msg;
+$response = $client->send_request($request);
+//$response = $client->publish($request);
+
+echo "client received response: ".PHP_EOL;
+print_r($response);
+echo "\n\n";
+
+echo $argv[0]." END".PHP_EOL;
+
+
+    
 ?>
