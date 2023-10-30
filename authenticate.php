5dd48de0cfbccaa51ff20b62fd9b73a4e0b0b6d8
dclerigo <140534171+dclerigo@users.noreply.github.com>
2023-10-29 23:22:24 -0400

    Add files via upload

--- authenticate.php
+++ authenticate.php
@@ -6,8 +6,8 @@ require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
 require_once('testRabbitMQ.ini');
 
-
-echo "First part of script";
+$username = $_POST['username'];
+$password = $_POST['password'];
 
 $client = new rabbitMQClient("testRabbitMQ.ini","testServer");
 if (isset($argv[1]))
@@ -19,12 +19,10 @@ else
   $msg = "test message";
 }
 
-echo "second part of script";
-
 $request = array();
 $request['type'] = "Login";
-$request['username'] = 'Bill';
-$request['password'] = 'password';
+$request['username'] = $username;
+$request['password'] = $password;
 $request['message'] = $msg;
 $response = $client->send_request($request);
 //$response = $client->publish($request);
