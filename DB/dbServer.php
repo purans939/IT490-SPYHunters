59246bfa45d32fdc946ac59732b3654f3c132acb
purans939 <106412399+purans939@users.noreply.github.com>
2023-10-30 23:47:45 -0400

    uploading working db files for midterm branch
    adding working db files for midterm:
    login/logout/sessionadd/sessiondel/validate session functions
    db scripts working as well

--- DB/dbServer.php
+++ DB/dbServer.php
@@ -6,7 +6,7 @@ require_once('rabbitMQLib.inc');
 
 function doLogin($username,$password)
 {
-$mydb = new mysqli('127.0.0.1','baseUser','12345','baseDB');
+	$mydb = new mysqli('127.0.0.1','baseUser','12345','baseDB');
 
 if ($mydb->errno != 0)
 {
@@ -14,20 +14,34 @@ if ($mydb->errno != 0)
 	exit(0);
 }
 
-echo "successfully connected to database".PHP_EOL;
+	echo "successfully connected to database".PHP_EOL;
 
-$query = "SELECT * FROM accounts WHERE username = 'username' AND password = 'password'";
+	$query = "SELECT * FROM accounts WHERE username = '$username' AND password = '$password';";
 
-$response = $mydb->query($query);
-if ($mydb->errno != 0)
-{
-	echo "failed to execute query:".PHP_EOL;
-	echo __FILE__.':'.__LINE__.":error: ".$mydb->error.PHP_EOL;
-	exit(0);
-}
+	$query2 = "SELECT password FROM accounts WHERE username = '$username';";
+	$stmt2 = $mydb->query($query2);	
 
-    return true;
-    //return false if not valid
+	$stmt = $mydb->query($query);
+	if ($stmt->num_rows>0) 
+	{	
+		echo "login success";
+		$msg = "login success";
+		return $msg;	
+		//exit(0);
+	}
+	else if ($password != $query2)
+	{
+		echo "incorrect pw";
+		$msg = "incorrect pw";
+		return $msg;
+	}
+	else
+        {
+                echo "username not found";
+                $msg = "username not found";
+                return $msg;
+
+        }
 }
 
 function createUser($username,$password)
@@ -43,21 +57,75 @@ if ($mydb->errno != 0)
 echo "successfully connected to database".PHP_EOL;
 
 $query = "INSERT INTO accounts (username, password) VALUES ('$username', '$password')";
-
 $response = $mydb->query($query);
+
+	echo "Account created";
+	return "Account has been created";
+ 	//return false if not valid
+}
+
+function addSession ($username) {
+
+$mydb = new mysqli('127.0.0.1','baseUser','12345','baseDB');
+
 if ($mydb->errno != 0)
 {
-        echo "failed to execute query:".PHP_EOL;
-        echo __FILE__.':'.__LINE__.":error: ".$mydb->error.PHP_EOL;
+        echo "failed to connect to database: ". $mydb->error . PHP_EOL;
         exit(0);
 }
 
-    return true;
-    //return false if not valid
+echo "successfully connected to database".PHP_EOL;
+
+$query = "INSERT INTO sessions (username) VALUES ('$username')";
+$response = $mydb->query($query);
+
 }
 
+function delSession ($username) {
 
+$mydb = new mysqli('127.0.0.1','baseUser','12345','baseDB');
 
+if ($mydb->errno != 0)
+{
+        echo "failed to connect to database: ". $mydb->error . PHP_EOL;
+        exit(0);
+}
+
+echo "successfully connected to database".PHP_EOL;
+
+$query = "DELETE FROM sessions WHERE username = $username";
+$response = $mydb->query($query);
+
+}
+
+function doValidate ($sessionID) {
+
+$mydb = new mysqli('127.0.0.1','baseUser','12345','baseDB');
+
+if ($mydb->errno != 0)
+{
+        echo "failed to connect to database: ". $mydb->error . PHP_EOL;
+        exit(0);
+}
+
+echo "successfully connected to database".PHP_EOL;
+
+$query = "SELECT * FROM sessions WHERE id = '$sessionID';";
+
+$stmt = $mydb->query($query);
+if ($stmt->num_rows>0) 
+{
+        echo "Validation successful".PHP_EOL;
+        //exit(0);
+}
+else
+{
+        echo "Session cannot be validated";
+}
+    return true;
+    //return false if not valid
+
+}
 
 function requestProcessor($request)
 {
