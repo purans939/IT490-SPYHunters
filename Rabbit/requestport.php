26bcd9125adcea81d74de1ea50089764309173df
purans939 <ps939@njit.edu>
2023-11-04 16:32:59 -0400

    adding client file for portfolio

---
+++ Rabbit/requestport.php
@@ -0,0 +1,32 @@
+#!/usr/bin/php
+<?php
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
+require_once('testRabbitMQ.ini');
+
+$client = new rabbitMQClient("testRabbitMQ.ini","testServer");
+if (isset($argv[1]))
+{
+  $msg = $argv[1];
+}
+else
+{
+  $msg = "test message";
+}
+
+$username = $_SESSION['username'];
+
+$request = array();
+$request['type'] = "portfolio";
+$request['username'] = $username;
+$response = $client->send_request($request);
+//$response = $client->publish($request);
+
+echo "client received response: ".PHP_EOL;
+print_r($response);
+echo "\n\n";
+
+echo $argv[0]." END".PHP_EOL;
+
+
